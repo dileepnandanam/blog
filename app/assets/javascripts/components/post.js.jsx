5ade7ebef5820473bda727f295972ef270e6d4d5
dileepsnandanam <dileepsnandanam@gmail.com>
2016-12-05 14:48:19 +0530

    dangerously set inner html

--- app/assets/javascripts/components/post.js.jsx
+++ app/assets/javascripts/components/post.js.jsx
@@ -1,7 +1,7 @@
 class Post extends React.Component {
 	constructor(props){
 		super(props)
-		alert(this.props.signed_in)
+		
 	}
 	render(){
 		return(
@@ -9,7 +9,7 @@ class Post extends React.Component {
 				<p className="post-created-at">{this.props.created_at}</p>
 				<div className="clearfix" />
 				<h1 className="post-heading">{this.props.title}</h1>
-				<p className="post-body">{this.props.body}</p>
+				<p className="post-body" dangerouslySetInnerHTML={{__html: this.props.body }}/ >
 				<CommentBox comments={this.props.comments} comments_url={this.props.comments_url} post_id={this.props.post_id} gravathar={this.props.gravathar} signed_in={this.props.signed_in}/>
 			</div>
 		)
